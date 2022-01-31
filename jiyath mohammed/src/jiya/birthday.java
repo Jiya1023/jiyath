dee62f635d0b0ce251f5b27b922b9ccdb88ad707
Ultimate <Ultimate@Ultimate-PC>
2022-01-31 21:53:18 +0530

    commits

---
+++ jiyath mohammed/src/jiya/birthday.java
@@ -0,0 +1,12 @@
+package jiya;
+
+public class birthday {
+	int a[]= {30, 3, 4, 12, 23, 10};
+	enum days{ sunday,monday,tuesday,wednesday,thursday,friday,saturday};
+	enum months{ january , febraury,march, june , august, september,october};
+	{
+		System.out.println(a[1]+""  +days.thursday+"" +months.march);
+		
+	}
+
+}
