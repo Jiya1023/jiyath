b78933a9a276bc711fac9cfe61a1ea1a750cf1ed
Ultimate <Ultimate@Ultimate-PC>
2022-02-06 22:30:27 +0530

    new one

---
+++ JIYAZ/src/birthday.java
@@ -0,0 +1,16 @@
+
+public class birthday {
+	
+	
+ 
+  enum days {monday ,tuesday ,wednesday, thursday ,friday , saturday,sunday};
+ 
+	  enum months{ jan ,feb,jun ,aug,sept,oct ,dec};	
+	
+	public static void main(String[]args)
+	{
+		int a[] = { 23, 4 ,5,10,13};
+		System.out.println(a[4]+"  "+ days.saturday+"  " +"  "+ months.jun);
+	}
+
+}
